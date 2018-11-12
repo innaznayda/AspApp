279c1bc61dcad6819cbdec494268810837897f6c
Inna Znaida <IZN@simcorp.com>
2018-11-12 15:12:32 +0200

    implement dbcontext

---
+++ FoodAspApplication/Data/FoodAspApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FoodAspApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodAspApplication.Data {
+    public class FoodAspApplicationDbContext : DbContext {
+
+        public FoodAspApplicationDbContext(DbContextOptions options) : base(options) {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
---
+++ FoodAspApplication/Services/SqlRestaurantData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FoodAspApplication.Data;
+using FoodAspApplication.Models;
+
+namespace FoodAspApplication.Services {
+    public class SqlRestaurantData : IRestaurantData {
+        private readonly FoodAspApplicationDbContext Context;
+
+        public SqlRestaurantData(FoodAspApplicationDbContext  context) {
+            Context = context;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant) {
+            Context.Restaurants.Add(newRestaurant);
+            Context.SaveChanges();
+            return newRestaurant;
+        }
+
+        public Restaurant Get(int id) {
+            return Context.Restaurants.FirstOrDefault(r=>r.Id==id);
+        }
+
+        public IEnumerable<Restaurant> GetAll() {
+            return Context.Restaurants.OrderBy(r=>r.Name);
+        }
+    }
+}
