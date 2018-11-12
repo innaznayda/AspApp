701fad795572dfd3787dd1753570bf6b9b4b7da1
Inna Znaida <IZN@simcorp.com>
2018-11-12 15:45:58 +0200

    ef up and running

--- FoodAspApplication/Startup.cs
+++ FoodAspApplication/Startup.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using FoodAspApplication.Services;
+using FoodAspApplication.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace FoodAspApplication {
     public class Startup {
+        private readonly IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
             services.AddSingleton<IGreeter, Greeter>();
-            services.AddSingleton<IRestaurantData, InMemoryRestaurantData>();
+            services.AddDbContext<FoodAspApplicationDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("FoodAspApplication")));
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
             services.AddMvc();
 
 
